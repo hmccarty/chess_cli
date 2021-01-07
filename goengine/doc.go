fdaae37fa75de3d70f0afbc35fee9c77c0f459b8
Harrison McCarty <mccarth@purdue.edu>
2021-01-07 03:09:33 -0500

    added init doc.go file for GoEngine

---
+++ goengine/doc.go
@@ -0,0 +1,13 @@
+/*
+ 
+GoEngine is a chess engine written purely in golang. This engine is
+written as part of a larger project, GoChess: a fully fleged chess
+application. GoEngine intends on featuring deep-learning evaluation
+and classical chess-programming techniques to make it similar modern
+engines, while still being simple enough to learn about how chess
+engines operate.
+
+For questions, contact Harrison McCarty (hmccarty@pm.me).
+
+*/
+package goengine
